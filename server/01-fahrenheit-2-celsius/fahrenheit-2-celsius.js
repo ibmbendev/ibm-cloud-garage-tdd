2e3791c170eaccad43a26ad392e828f81f145da3
ben supanga <ibmbendev@gmail.com>
2020-03-19 13:19:27 +0800

    Pass the second test

--- server/01-fahrenheit-2-celsius/fahrenheit-2-celsius.js
+++ server/01-fahrenheit-2-celsius/fahrenheit-2-celsius.js
@@ -1,5 +1,6 @@
-const fahrenheitToCelsius = () => { 
+const fahrenheitToCelsius = (fahrenheit) => (fahrenheit - 32) * 5 / 9;
+/*const fahrenheitToCelsius = () => { 
     return 0;
-};
+};*/
 
 module.exports = { fahrenheitToCelsius };
