acd4cb98ae960dece15f7b8b7c21762c8e589d29
ben supanga <ibmbendev@gmail.com>
2020-03-19 17:39:14 +0800

    stack pops two items

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -1,23 +1,23 @@
 const stackFactory = () => {
   //let empty = true;
-  let count = 0;
-  let element;
+  //let count = 0;
+  let elements = [];
   return {
-    isEmpty: () => count === 0,
-    size: () => count,
+    isEmpty: () => elements.length === 0,
+    size: () => elements.length,
     push: (ele) => {
       //empty = false;
-      if (count === 2) throw new Error('capacity overflow error');
+      if (elements.length === 2) throw new Error('capacity overflow error');
 
-      count += 1;
-      element = ele;
+     // count += 1;
+      elements.push(ele);
 
     },
     pop: () => {  
-      empty = true;
-      if (count === 0) throw new Error('capacity underflow error')
-      count -= 1;
-      return element;
+      //empty = true;
+      if (elements.length === 0) throw new Error('capacity underflow error')
+      //count -= 1;
+      return elements.pop();
     }
   }
 };
@@ -78,6 +78,11 @@ describe('a stack', () => {
     stack.push('a');
     expect(stack.pop()).toBe('a');
   });
-  it.todo('pops two items with the most recent first');
+  it('pops two items with the most recent first', () => {
+    stack.push('1');
+    stack.push('2')
+    expect(stack.pop()).toBe('2');
+    expect(stack.pop()).toBe('1');
+  });
   it.todo('accepts only a positive capacity');
 });
