8df8a532ad079014038d9ffd5e5b5173ecec813e
ben supanga <ibmbendev@gmail.com>
2020-03-19 16:56:45 +0800

    introduce guard clause to get to green bar

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -1,4 +1,9 @@
-let isPalindrome = (text) => text === text.split('').reverse().join('');
+/*let isPalindrome = (text) => text === text.split('').reverse().join('');
+*/
+let isPalindrome = (phrase) => {
+  if (phrase.trim() === '') return false;
+  return phrase === phrase.split('').reverse().join('')
+};
 
 describe('the palindrome canary spec', () => {
   it('shows the infrastructure works', () => {
@@ -10,13 +15,20 @@ describe('the palindrome canary spec', () => {
   it('false for dude', () => {
     expect(isPalindrome('dude')).toBe(false);
   });
+  /*
   it('true for mom mom', () => {
     expect(isPalindrome('mom mom')).toBe(true);
-  });
+  });*/
   it('false for dad mom', () => {
     expect(isPalindrome('dad mom')).toBe(false);
   }); 
-  it.todo ('true for whitespace');
+  /*
+  it('true for whitespace', () => {
+    expect(isPalindrome('   ')).toBe(true);
+  });*/
+  it('false for whitespace', () => {
+    expect(isPalindrome('   ')).toBe(false);
+  });
   it.todo ('error for empty string');
   it.todo ('error for not a string');
 
