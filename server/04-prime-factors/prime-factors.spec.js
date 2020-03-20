76d5954a51f8a0978354a3f1e226ae775f8b1bb9
ben supanga <ibmbendev@gmail.com>
2020-03-20 12:13:58 +0800

    prime factors for 3

--- server/04-prime-factors/prime-factors.spec.js
+++ server/04-prime-factors/prime-factors.spec.js
@@ -19,6 +19,7 @@ describe('a prime factors function should', () => {
   it('return 3 for 3', () => {
     expect(findPrimeFactors(3)).toEqual([3]);
   });
+  
   it.todo('return 2, 2 for 4');
   it.todo('return 5 for 5');
   it.todo('return 2, 3 for 6');
